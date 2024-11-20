bda09e96ad48c34e26bfaa639a79bb739b4468a1
Ahmed <ahmed.khalaf.1088@gmail.com>
2024-11-20 02:54:14 +0100

    Building database by supabase and create a file for supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://nhdwwsrdxnsavyfxxxee.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oZHd3c3JkeG5zYXZ5Znh4eGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMjU3MjQsImV4cCI6MjA0NzYwMTcyNH0.ny_1Kmn8pkias7b0-pDXMGczNHwkys68hy0zN6Qjz3I";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
