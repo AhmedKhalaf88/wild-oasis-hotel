bccbcb3a3336c0f83579d4339a205bf25fe362c7
Ahmed <ahmed.khalaf.1088@gmail.com>
2025-01-06 05:44:42 +0100

    Fixing some bugs

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import Uploader from "../data/Uploader";
 
 const StyledSidbar = styled.aside`
   background-color: var(--color-grey-0);
@@ -18,8 +17,6 @@ function Sidebar() {
     <StyledSidbar>
       <Logo />
       <MainNav />
-
-      <Uploader />
     </StyledSidbar>
   );
 }
